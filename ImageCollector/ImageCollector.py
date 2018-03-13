3030647ff986923f2aecca990e682aab23eeb74e
Kasper <kasper_claessens@hotmail.com>
2018-03-13 15:39:32 +0100

    Added GPIO inputs and some TODO comments

--- ImageCollector/ImageCollector.py
+++ ImageCollector/ImageCollector.py
@@ -1,24 +1,56 @@
 from time import sleep
-import time
 from picamera import PiCamera
+import RPi.GPIO as GPIO
+
+def init_GPIO():
+    GPIO.setmode(GPIO.BCM) #BCM pin numbering
+    GPIO.setup(3,GPIO.IN) #Use pin3 as input
+
 
 def captureImage():
+
+    active = True
     camera = PiCamera()
     camera.resolution = (320, 240)
     camera.start_preview()
     sleep(2)
-    while (active):
-        timestamp = time.strftime("%d-$m_%H:%M:%S")
+
+    while active:
+
+
+        timestamp = time.strftime("%d-$m_%H:%M:%S") #TODO:Add milliseconds
         camera.capture('/media/pi/RASPBERRY/img'+timestamp+'.jpeg')
-    camera.stop_preview()
+        #TODO:Steeringinput capture
+        #TODO:Save path & steering input to csv file
+
+        active = GPIO.input(3) #Check pause status
+        while nowTime < captureTime + 1:
+            nowTime =
+            if active: #break if pause button is pressed
+                active  = False
+                #TODO:wait for depress
+                break
+
+    while not active: #Loop during pause status
+        active = GPIO.input(3)
+        if active: #break if pause button is pressed again (back to main loop)
+            # TODO:wait for depress
+            break
 
 
-def activateCamera():
-    global active
-    active = True
-    captureImage()
+
+# def activateCamera():
+#     global active
+#     active = True
+#     captureImage()
+#
+#
+# def deactivateCamera():
+#     global active
+#     active = False
+
+if __name__ == "__main__":
+    while True:
+        captureImage()
 
 
-def deactivateCamera():
-    global active
-    active = False
