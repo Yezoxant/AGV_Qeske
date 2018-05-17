d1ad9ed7ecafa3785f164b32b67f9ef76e20acbd
Kasper <kasper_claessens@hotmail.com>
2018-05-17 15:44:13 +0200

    loadandnormalize

---
+++ load_images.py
@@ -0,0 +1,20 @@
+import csv
+import numpy as np
+import cv2
+import os
+
+
+input = []
+target = []
+csv_file_name = "augdata.csv"
+with open(csv_file_name) as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        img = cv2.imread(row[0])
+        ang = row[2]
+        img = np.array(img, dtype=np.float64)
+        img = img[:,:,:] - 128
+        img = img[:,:,:] *(1/128)
+
+        input.append(img)
+        target.append(ang)
