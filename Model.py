1cdb601198e87e9efab57a87bd2b2cc24a6fd468
Brandon <brandon94kleijnen@live.nl>
2018-05-17 16:23:51 +0200

    updating model and images

--- Model.py
+++ Model.py
@@ -1,36 +1,37 @@
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import csv
 import cv2
-import random
+#import random
 import matplotlib as mpl
-import matplotlib.image as mpimg
+#import matplotlib.image as mpimg
 mpl.use('Agg')
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import argparse
 import os
-import time
+#import time
 
-from os import path
-from collections import defaultdict
-from numpy import sin, cos
-from scipy.misc import imread, imresize, imsave
-from scipy import ndimage
-from scipy import misc
+#from os import path
+# from collections import defaultdict
+# from numpy import sin, cos
+# from scipy.misc import imread, imresize, imsave
+# from scipy import ndimage
+# from scipy import misc
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.models import Model, Sequential
-from keras.preprocessing import image
+#from keras.preprocessing import image
 from keras.layers import Dense, Flatten, Dropout, Input, BatchNormalization
 from keras.layers import Convolution2D, MaxPooling2D, AveragePooling2D, GlobalAveragePooling2D
-from keras.layers.advanced_activations import ELU
+#from keras.layers.advanced_activations import ELU
 from keras.optimizers import Adam
-from keras import backend as K
+#from keras import backend as K
 
 
 def build_cnn():
 
-    img_input = Input(200,66,3)
+    img_input = Input(shape=(66,200,3))
+
 
     # Layer 1
     x = Convolution2D(24, 5, 5, activation='elu', subsample=(2, 2), border_mode='valid', init='he_normal')(img_input)
@@ -74,7 +75,7 @@ def normalize_input(x):
     return (x - 128.) / 128.
 
 def data_setup():
-    os.chdir("DataAugmentation/Dataset_1/output_1/")
+    os.chdir("output_1/")
     images = []
     steering = []
 
@@ -97,10 +98,10 @@ def data_setup():
 
 def main():
     parser = argparse.ArgumentParser(prog="Autonomous driving cart")
-    parser.add_argument('batch_size', type=int, default=64, help='training batch size')
-    parser.add_argument('epoch', type=int, default=10, help='# of training epoch')
-    parser.add_argument('train_samples', type=int,help='# of train samples per epoch')
-    parser.add_argument('val_samples',type=int,help='# of validation samples')
+    # parser.add_argument('batch_size', type=int, default=64, help='training batch size')
+    # parser.add_argument('epoch', type=int, default=10, help='# of training epoch')
+    # parser.add_argument('train_samples', type=int,help='# of train samples per epoch')
+    # parser.add_argument('val_samples',type=int,help='# of validation samples')
     args = parser.parse_args()
 
     model = build_cnn()
@@ -115,7 +116,10 @@ def main():
 
     model.fit(images,
               steering,
-              args.batch_size,
-              args.epoch,verbose=2,
-              callbacks=callbacks,
+              batch_size=64#args.batch_size,
+              ,epochs=5#args.epoch,verbose=2,
+              ,callbacks=callbacks,
               validation_split=0.9)
+
+if __name__ == '__main__':
+    main()
