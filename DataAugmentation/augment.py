49fe348fd03bf49dae9627d7641a20924b263dc0
Kasper <kasper_claessens@hotmail.com>
2018-05-17 10:57:44 +0200

    augment bugfixes, filters 0 throttle, transforms to hsv colorspace

--- DataAugmentation/augment.py
+++ DataAugmentation/augment.py
@@ -16,9 +16,10 @@ class AugmentDataset():
             reader = csv.reader(csv_file)
             for row in reader:
                 # Load all source images and steering inputs. images is a list of lists [[[cv2 img_matrix],throttle,steering],...]
-                self.images.append([cv2.imread(row[0]),row[1],row[2]])
+                if row[1] != "0.0": #filter out 0 throttle images
+                    self.images.append([cv2.imread(row[0]),row[1],row[2]])
 
-        if self.images == None:
+        if self.images[0] == None:
             print("No images imported")
 
 
@@ -30,14 +31,22 @@ class AugmentDataset():
         writer = csv.writer(augdata_csv)
         count = 0
 
-        base_chance = 10 #chance of applying an augment is 1/base_chance
+        base_chance = 2 #chance of applying an augment is 1/base_chance
+
+        for image in self.images:
+            img = image[0]
+            img_name = "image" + str(count) + ".jpeg"
+            self.save_image(img_name, img)
+            writer.writerow([img_name, image[1], image[2]])
+            count += 1
+
         if shadows:
 
             for image in self.images:
                 if np.random.randint(0, base_chance) == 0:
                     out_img = self.add_random_shadow(image[0])
                     img_name = "image" + str(count) + ".jpeg"
-                    cv2.imwrite(img_name,out_img)
+                    self.save_image(img_name,out_img)
                     writer.writerow([img_name,image[1],image[2]])
                     count += 1
 
@@ -47,7 +56,7 @@ class AugmentDataset():
                 if np.random.randint(0, base_chance) == 0:
                     out_img = self.augment_brightness_camera_images(image[0])
                     img_name = "image"+str(count)+".jpeg"
-                    cv2.imwrite(img_name,out_img)
+                    self.save_image(img_name,out_img)
                     writer.writerow([img_name, image[1], image[2]])
                     count += 1
 
@@ -55,10 +64,10 @@ class AugmentDataset():
 
             for image in self.images:
                 if np.random.randint(0, base_chance) == 0:
-                    out_img = self.augment_brightness_camera_images(image[0])
+                    out_img = self.hflip(image[0])
                     img_name = "image"+str(count)+".jpeg"
-                    cv2.imwrite(img_name,out_img)
-                    writer.writerow([img_name, image[1], float(image[2])*-1]) #invert steering
+                    self.save_image(img_name,out_img)
+                    writer.writerow([img_name, image[1], float(image[2])*-1]) #invert steering angle
                     count += 1
 
         if horizontal_translate:
@@ -67,7 +76,7 @@ class AugmentDataset():
                 if np.random.randint(0, base_chance) == 0:
                     out_img = self.trans_image(image[0],image[2],0.01)
                     img_name = "image" + str(count) + ".jpeg"
-                    cv2.imwrite(img_name, out_img)
+                    self.save_image(img_name, out_img)
                     writer.writerow([img_name, image[1], float(image[2])*-1])
                     count += 1
         if blur:
@@ -75,19 +84,25 @@ class AugmentDataset():
                 if np.random.randint(0, base_chance) == 0:
                     out_img = self.blur_image(image[0])
                     img_name = "image" + str(count) + ".jpeg"
-                    cv2.imwrite(img_name, out_img)
+                    self.save_image(img_name, out_img)
                     writer.writerow([img_name, image[1], float(image[2])*-1])
                     count += 1
 
         augdata_csv.close()
 
+
+    def save_image(self,name,image):
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        cv2.imwrite(name, image)
+
     def blur_image(self,image):
-        image1 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
         kernelsize = np.random.randint(3,6)
-        image_out = cv2.blur(image1,(kernelsize, kernelsize))
+        image_out = cv2.blur(image,(kernelsize, kernelsize))
         return image_out
 
     def augment_brightness_camera_images(self,image):
+
         image1 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
         image1 = np.array(image1, dtype=np.float64)
         random_bright = .5 + np.random.uniform()
@@ -96,6 +111,7 @@ class AugmentDataset():
         image1 = np.array(image1, dtype=np.uint8)
         image1 = cv2.cvtColor(image1, cv2.COLOR_HSV2RGB)
         return image1
+
 
 
     def trans_image(self,image, steer, trans_range):
@@ -117,29 +133,26 @@ class AugmentDataset():
         top_x = 0
         bot_x = 160
         bot_y = 320 * np.random.uniform()
-        image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-        shadow_mask = 0 * image_hsv[:, :, 1]
+        image_hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+        shadow_mask = 0 * image_hls[:, :, 1]
         X_m = np.mgrid[0:image.shape[0], 0:image.shape[1]][0]
         Y_m = np.mgrid[0:image.shape[0], 0:image.shape[1]][1]
+
         shadow_mask[((X_m - top_x) * (bot_y - top_y) - (bot_x - top_x) * (Y_m - top_y) >= 0)] = 1
         # random_bright = .25+.7*np.random.uniform()
         random_bright = .5
         cond1 = shadow_mask == 1
         cond0 = shadow_mask == 0
         if np.random.randint(2) == 1:
-            image_hsv[:, :, 1][cond1] = image_hsv[:, :, 1][cond1] * random_bright
+            image_hls[:, :, 1][cond1] = image_hls[:, :, 1][cond1] * random_bright
         else:
-            image_hsv[:, :, 1][cond0] = image_hsv[:, :, 1][cond0] * random_bright
-        image = cv2.cvtColor(image_hsv, cv2.COLOR_RGB2HSV)
+            image_hls[:, :, 1][cond0] = image_hls[:, :, 1][cond0] * random_bright
+        image = cv2.cvtColor(image_hls, cv2.COLOR_HLS2RGB)
         return image
 
-
     def hflip(self,image):
-        image_yuv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-        out_img = cv2.flip(image_yuv,0)
+        out_img = cv2.flip(image,1)
         return out_img
-
-
 
 
 if __name__ == "__main__":
