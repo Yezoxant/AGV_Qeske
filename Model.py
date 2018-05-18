f9523bbfa5ec157541d03bd212d8e7f033a6a9b1
Brandon <brandon94kleijnen@live.nl>
2018-05-18 13:37:05 +0200

    small changes to train the model

--- Model.py
+++ Model.py
@@ -32,7 +32,6 @@ def build_cnn():
 
     img_input = Input(shape=(66,200,3))
 
-
     # Layer 1
     x = Convolution2D(24, 5, 5, activation='elu', subsample=(2, 2), border_mode='valid', init='he_normal')(img_input)
     
@@ -52,27 +51,24 @@ def build_cnn():
     y = Flatten()(x)
 
     # FC 1
-    y = Dense(100, activation='elu', init='he_normal')(y)
+    y = Dense(100, activation='elu', kernel_initializer='he_normal')(y)
 
     # FC 2
-    y = Dense(50, activation='elu', init='he_normal')(y)
+    y = Dense(50, activation='elu', kernel_initializer='he_normal')(y)
 
     # FC 3
-    y = Dense(10, activation='elu', init='he_normal')(y)
+    y = Dense(10, activation='elu', kernel_initializer='he_normal')(y)
 
     # Output Layer
-    y = Dense(1, init='he_normal')(y)
+    y = Dense(1, kernel_initializer='he_normal')(y)
 
     model = Model(input=img_input, output=y)
     for layer in model.layers[:3]:
         layer.trainable = False
-    model.compile(optimizer=Adam(lr=1e-4), loss = 'mse')
+    model.compile(optimizer=Adam(lr=1e-4), loss = 'mse', metrics=['accuracy'])
     model.load_weights("model.h5")
 
     return model
-
-def normalize_input(x):
-    return (x - 128.) / 128.
 
 def data_setup():
     os.chdir("output_1/")
@@ -97,29 +93,29 @@ def data_setup():
     return npdata
 
 def main():
+    #arguments to change for running
     parser = argparse.ArgumentParser(prog="Autonomous driving cart")
-    # parser.add_argument('batch_size', type=int, default=64, help='training batch size')
-    # parser.add_argument('epoch', type=int, default=10, help='# of training epoch')
-    # parser.add_argument('train_samples', type=int,help='# of train samples per epoch')
-    # parser.add_argument('val_samples',type=int,help='# of validation samples')
+    parser.add_argument('--batch_size', type=int, default=64, help='training batch size')
+    parser.add_argument('--epoch', type=int, default=10, help='# of training epoch')
     args = parser.parse_args()
+    
 
     model = build_cnn()
-
-
+    #gather all the data
     npdata = data_setup()
     images = npdata[0]
     steering = npdata[1]
+    os.chdir("../")
 
-    callbacks = [EarlyStopping(monitor='val_loss', patience=5, verbose=0), 
-                    ModelCheckpoint('model.h5', monitor='val_loss', verbose=0, save_best_only=True,save_weights_only=True,mode='min')]
-
+    callbacks = [ModelCheckpoint('model.h5', monitor='val_loss', verbose=0, save_best_only=True,save_weights_only=True,mode='min')]
+    #run the model
     model.fit(images,
               steering,
-              batch_size=64#args.batch_size,
-              ,epochs=5#args.epoch,verbose=2,
+              batch_size=args.batch_size
+              ,epochs=args.epoch
+              ,verbose=1
               ,callbacks=callbacks,
-              validation_split=0.9)
+              validation_split=0.1)
 
 if __name__ == '__main__':
     main()
